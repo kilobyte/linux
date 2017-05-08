7ca0ce02ec2b82595f3cd12ec0744aa4d79b9bf7
Daichou <tommy0705c@gmail.com>
2017-05-08 10:10:02 +0800

    Btrfs: remove obsolete FIXMEs in qgroup ioctls
    These FIXMEs were already addressed in 2013. All functions check for
    qgroup existence:
    
    * btrfs_add_qgroup_relation
    * btrfs_ioctl_qgroup_create
    * btrfs_limit_qgroup
    * btrfs_del_qgroup_relation
    
    Signed-off-by: Daichou <tommy0705c@gmail.com>
    [ enhance and reformat changelog ]
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -4898,7 +4898,6 @@ static long btrfs_ioctl_qgroup_assign(struct file *file, void __user *arg)
 		goto out;
 	}
 
-	/* FIXME: check if the IDs really exist */
 	if (sa->assign) {
 		ret = btrfs_add_qgroup_relation(trans, fs_info,
 						sa->src, sa->dst);
@@ -4957,7 +4956,6 @@ static long btrfs_ioctl_qgroup_create(struct file *file, void __user *arg)
 		goto out;
 	}
 
-	/* FIXME: check if the IDs really exist */
 	if (sa->create) {
 		ret = btrfs_create_qgroup(trans, fs_info, sa->qgroupid);
 	} else {
@@ -5011,7 +5009,6 @@ static long btrfs_ioctl_qgroup_limit(struct file *file, void __user *arg)
 		qgroupid = root->root_key.objectid;
 	}
 
-	/* FIXME: check if the IDs really exist */
 	ret = btrfs_limit_qgroup(trans, fs_info, qgroupid, &sa->lim);
 
 	err = btrfs_end_transaction(trans);
