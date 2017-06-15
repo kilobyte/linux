a5b166769b00100a621efd0d9d3f0581457c3d27
David Sterba <dsterba@suse.com>
2017-06-15 16:04:26 +0200

    btrfs: account as waiting for IO, while waiting fot the flush bio completion
    Similar to what submit_bio_wait does, we should account for IO while
    waiting for a bio completion. This has marginal visible effects, flush
    bio is short-lived.
    
    Reviewed-by: Anand Jain <anand.jain@oracle.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -3529,7 +3529,7 @@ static int wait_dev_flush(struct btrfs_device *device)
 		return 0;
 
 	device->flush_bio_sent = 0;
-	wait_for_completion(&device->flush_wait);
+	wait_for_completion_io(&device->flush_wait);
 
 	if (bio->bi_error) {
 		ret = bio->bi_error;
