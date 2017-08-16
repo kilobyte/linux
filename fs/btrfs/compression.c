78ef11e90cbe6ab3cd297513ba179cb73b45609a
Adam Borowski <kilobyte@angband.pl>
2017-08-16 14:46:15 +0200

    btrfs: allow setting zlib compression level via :9
    This is bikeshedding, but it seems people are drastically more likely to
    understand "zlib:9" as compression level rather than an algorithm version
    compared to "zlib9".
    
    Signed-off-by: Adam Borowski <kilobyte@angband.pl>

--- fs/btrfs/compression.c
+++ fs/btrfs/compression.c
@@ -1112,6 +1112,8 @@ unsigned int btrfs_compress_str2level(const char *str)
 
 	if ('1' <= str[4] && str[4] <= '9' )
 		return str[4] - '0';
+	if (str[4] == ':' && '1' <= str[5] && str[5] <= '9')
+		return str[5] - '0';
 
 	return 0;
 }
